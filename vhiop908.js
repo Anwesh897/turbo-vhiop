18a4150340e30656ae1692dc56a5ab254264dc7d
Anwesh <anwesh897@gmail.com>
2019-07-04 21:08:18 +0530

    commit

--- vhiop908.js
+++ vhiop908.js
@@ -6,8 +6,7 @@ rotateCarousel();
 function rotateCarousel() {
   var angle = selectedIndex / cellCount * -360;
   carousel.style.transform = 'translateZ(-350px) rotateY(' + angle + 'deg)';
-  /*var slides = document.getElementsByClassName('carousel__cell');
-  var dots = document.getElementsByClassName('dot');
+  var slides = document.getElementsByClassName('carousel__cell');  ////  ////  ////image slider from w3school
   for(i=0;i<slides.length;i++){
       slides[i].style.display='block';
   }
@@ -15,12 +14,30 @@ function rotateCarousel() {
   if(selectedIndex>slides.length){
       selectedIndex = 1;
   }
-  for(i=1;i<dots.length;i++){
-      dots[i].style.background = dots[i].style.background.replace('black',' #bbb');
-  }
-  slides[selectedIndex-1].style.display = 'block';
-  dots[selectedIndex-1].style.background ='black';
-  setTimeout(rotateCarousel,1000);*/
+  slides[selectedIndex+1].style.display = 'block';
+  setTimeout(rotateCarousel,1000);
+    function bgs(){
+    var bgar = ['linear-gradient(to right,black,indigo,indigo,black)',
+    'linear-gradient(to right,white,indigo,indigo,black)',
+    'linear-gradient(to right,indigo,white,indigo,black)',
+    'linear-gradient(to right,black,indigo,white,indigo)',
+    'linear-gradient(to right,black,indigo,indigo,white)',
+    'linear-gradient(to right,black,indigo,indigo,black)']
+        var bgs = document.getElementById('id01');
+        bgs.style.background = bgar[1];
+        setTimeout(function(){
+            bgs.style.background = bgar[2];
+        },50); 
+        setTimeout(function(){
+            bgs.style.background = bgar[3];
+        },100);
+        setTimeout(function(){
+            bgs.style.background = bgar[4];
+        },150);
+        setTimeout(function(){
+            bgs.style.background = bgar[5];
+        },200);
+    }bgs();
 }
 
 var prevButton = document.querySelector('.previous-button');
@@ -28,12 +45,12 @@ prevButton.addEventListener( 'click', function() {
   selectedIndex--;
   rotateCarousel();
   function bgs(){
-    var bgar = ['linear-gradient(to right,black,indigo,white,indigo,black)',
-    'linear-gradient(to right,black,indigo,white,indigo,black)',
-    'linear-gradient(to right,white,indigo,white,indigo,black)',
-    'linear-gradient(to right,indigo,white,white,indigo,black)',
-    'linear-gradient(to right,black,indigo,white,white,indigo)',
-    'linear-gradient(to right,black,indigo,white,indigo,white)']
+    var bgar = ['linear-gradient(to right,black,indigo,indigo,black)',
+    'linear-gradient(to right,black,indigo,indigo,black)',
+    'linear-gradient(to right,white,indigo,indigo,black)',
+    'linear-gradient(to right,indigo,white,indigo,black)',
+    'linear-gradient(to right,black,indigo,white,indigo)',
+    'linear-gradient(to right,black,indigo,indigo,white)']
         var bgs = document.getElementById('id01');
         bgs.style.background = bgar[5];
         setTimeout(function(){
@@ -56,12 +73,12 @@ nextButton.addEventListener( 'click', function() {
   selectedIndex++;
   rotateCarousel();
   function bgs(){
-    var bgar = ['linear-gradient(to right,black,indigo,white,indigo,black)',
-    'linear-gradient(to right,white,indigo,white,indigo,black)',
-    'linear-gradient(to right,indigo,white,white,indigo,black)',
-    'linear-gradient(to right,black,indigo,white,white,indigo)',
-    'linear-gradient(to right,black,indigo,white,indigo,white)',
-    'linear-gradient(to right,black,indigo,white,indigo,black)']
+    var bgar = ['linear-gradient(to right,black,indigo,indigo,black)',
+    'linear-gradient(to right,white,indigo,indigo,black)',
+    'linear-gradient(to right,indigo,white,indigo,black)',
+    'linear-gradient(to right,black,indigo,white,indigo)',
+    'linear-gradient(to right,black,indigo,indigo,white)',
+    'linear-gradient(to right,black,indigo,indigo,black)']
         var bgs = document.getElementById('id01');
         bgs.style.background = bgar[1];
         setTimeout(function(){
