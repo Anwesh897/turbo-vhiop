4f3b4fce5f671ad63fd804d255449c3e02886a40
Anwesh897 <anwesh897@gmail.com>
2019-07-24 13:43:26 +0530

    'commit'

--- vhiop908.js
+++ vhiop908.js
@@ -6,38 +6,38 @@ rotateCarousel();
 function rotateCarousel() {
   var angle = selectedIndex / cellCount * -360;
   carousel.style.transform = 'translateZ(-350px) rotateY(' + angle + 'deg)';
-  var slides = document.getElementsByClassName('carousel__cell');  ////  ////  ////image slider from w3school
-  for(i=0;i<slides.length;i++){
-      slides[i].style.display='block';
-  }
-  selectedIndex++;
-  if(selectedIndex>slides.length){
-      selectedIndex = 1;
-  }
-  slides[selectedIndex+1].style.display = 'block';
-  setTimeout(rotateCarousel,1000);
-    function bgs(){
-    var bgar = ['linear-gradient(to right,black,indigo,indigo,black)',
-    'linear-gradient(to right,white,indigo,indigo,black)',
-    'linear-gradient(to right,indigo,white,indigo,black)',
-    'linear-gradient(to right,black,indigo,white,indigo)',
-    'linear-gradient(to right,black,indigo,indigo,white)',
-    'linear-gradient(to right,black,indigo,indigo,black)']
-        var bgs = document.getElementById('id01');
-        bgs.style.background = bgar[1];
-        setTimeout(function(){
-            bgs.style.background = bgar[2];
-        },50); 
-        setTimeout(function(){
-            bgs.style.background = bgar[3];
-        },100);
-        setTimeout(function(){
-            bgs.style.background = bgar[4];
-        },150);
-        setTimeout(function(){
-            bgs.style.background = bgar[5];
-        },200);
-    }bgs();
+//   var slides = document.getElementsByClassName('carousel__cell');  ////  ////  ////image slider from w3school
+//   for(i=0;i<slides.length;i++){
+//       slides[i].style.display='block';
+//   }
+//   selectedIndex++;
+//   if(selectedIndex>slides.length){
+//       selectedIndex = 1;
+//   }
+//   slides[selectedIndex+1].style.display = 'block';
+//   setTimeout(rotateCarousel,1000);
+//     function bgs(){
+//     var bgar = ['linear-gradient(to right,black,black,black,black)',
+//     'linear-gradient(to right,white,black,black,black)',
+//     'linear-gradient(to right,black,white,black,black)',
+//     'linear-gradient(to right,black,black,white,black)',
+//     'linear-gradient(to right,black,black,black,white)',
+//     'linear-gradient(to right,black,black,black,black)']
+//         var bgs = document.getElementById('id01');
+//         bgs.style.background = bgar[1];
+//         setTimeout(function(){
+//             bgs.style.background = bgar[2];
+//         },50); 
+//         setTimeout(function(){
+//             bgs.style.background = bgar[3];
+//         },100);
+//         setTimeout(function(){
+//             bgs.style.background = bgar[4];
+//         },150);
+//         setTimeout(function(){
+//             bgs.style.background = bgar[5];
+//         },200);
+//     };
 }
 
 var prevButton = document.querySelector('.previous-button');
@@ -45,12 +45,12 @@ prevButton.addEventListener( 'click', function() {
   selectedIndex--;
   rotateCarousel();
   function bgs(){
-    var bgar = ['linear-gradient(to right,black,indigo,indigo,black)',
-    'linear-gradient(to right,black,indigo,indigo,black)',
-    'linear-gradient(to right,white,indigo,indigo,black)',
-    'linear-gradient(to right,indigo,white,indigo,black)',
-    'linear-gradient(to right,black,indigo,white,indigo)',
-    'linear-gradient(to right,black,indigo,indigo,white)']
+    var bgar = ['linear-gradient(to right,black,black,black,black)',
+    'linear-gradient(to right,black,black,black,black)',
+    'linear-gradient(to right,white,black,black,black)',
+    'linear-gradient(to right,black,white,black,black)',
+    'linear-gradient(to right,black,black,white,black)',
+    'linear-gradient(to right,black,black,black,white)']
         var bgs = document.getElementById('id01');
         bgs.style.background = bgar[5];
         setTimeout(function(){
@@ -73,12 +73,12 @@ nextButton.addEventListener( 'click', function() {
   selectedIndex++;
   rotateCarousel();
   function bgs(){
-    var bgar = ['linear-gradient(to right,black,indigo,indigo,black)',
-    'linear-gradient(to right,white,indigo,indigo,black)',
-    'linear-gradient(to right,indigo,white,indigo,black)',
-    'linear-gradient(to right,black,indigo,white,indigo)',
-    'linear-gradient(to right,black,indigo,indigo,white)',
-    'linear-gradient(to right,black,indigo,indigo,black)']
+    var bgar = ['linear-gradient(to right,black,black,black,black)',
+    'linear-gradient(to right,white,black,black,black)',
+    'linear-gradient(to right,black,white,black,black)',
+    'linear-gradient(to right,black,black,white,black)',
+    'linear-gradient(to right,black,black,black,white)',
+    'linear-gradient(to right,black,black,black,black)']
         var bgs = document.getElementById('id01');
         bgs.style.background = bgar[1];
         setTimeout(function(){
